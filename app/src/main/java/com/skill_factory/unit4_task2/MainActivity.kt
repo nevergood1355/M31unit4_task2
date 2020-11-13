fef44351f42102e8807dafcc8cd112ac7a3e997e
vurtune <kostya.urenevar@gmail.com>
2020-11-13 16:34:59 +0400

    New commit

--- app/src/main/java/com/skill_factory/unit4_task2/MainActivity.kt
+++ app/src/main/java/com/skill_factory/unit4_task2/MainActivity.kt
@@ -1,7 +1,23 @@
 package com.skill_factory.unit4_task2
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.transition.Explode
+import android.transition.Transition
+import android.transition.TransitionManager
+import android.transition.Visibility.MODE_IN
+import android.transition.Visibility.MODE_OUT
+import android.util.Log
+import android.view.View
+import android.view.View.*
+import android.view.ViewGroup
+import android.widget.TableRow
+import androidx.annotation.IntDef
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.transition.addListener
+import androidx.core.view.children
+import androidx.core.view.forEach
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var adapter: MyAdapter
@@ -9,6 +25,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val emptyAdapter = MyAdapter(0)
+        adapter = MyAdapter(9) {
+            val viewRect = Rect()
+            it.getGlobalVisibleRect(viewRect)
+            val explodeOut = Explode().apply {
+                mode = MODE_OUT; duration = 1200;
+                epicenterCallback = object : Transition.EpicenterCallback() {
+                    override fun onGetEpicenter(transition: Transition?): Rect {
+                        return viewRect
+                    }
+                }
+            }
 
+            val explodeIn = Explode().apply {
+                mode = MODE_IN; duration = 1200;
+                epicenterCallback = object : Transition.EpicenterCallback() {
+                    override fun onGetEpicenter(transition: Transition?): Rect {
+                        return viewRect
+                    }
+                }
+            }
+            explodeOut.addListener({ _ ->
+                TransitionManager.beginDelayedTransition(recycler_view, explodeIn)
+                recycler_view.adapter = adapter
+                it.setBackgroundResource(R.color.colorAccent)
+            })
+            TransitionManager.beginDelayedTransition(recycler_view, explodeOut)
+            recycler_view.adapter = emptyAdapter
+            it.setBackgroundResource(R.color.colorPrimary)
+        }
+
+        recycler_view.adapter = adapter
     }
 }
